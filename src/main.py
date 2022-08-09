27b461e0e4e27982ddc273f8bf98135383a3a740
ValeriaZampetti <vzampetti@correo.unimet.edu.ve>
2022-08-09 04:45:45 +0000

    listo

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, CharacterFavorite, PlanetFavorite
+
 #from models import Person
 
 app = Flask(__name__)
@@ -33,11 +34,178 @@ def sitemap():
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
 
-    return jsonify(response_body), 200
+    return jsonify(all_users), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+
+    request_body_user = request.get_json()
+    user1 = User(
+        first_name=request_body_user["first_name"],
+        email=request_body_user["email"], 
+        password=request_body_user["password"]
+        )
+    db.session.add(user1)
+    db.session.commit()
+    return jsonify(request_body_user), 200
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def modify_user(user_id):
+
+    request_body_user = request.get_json()
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException('User noooot found', status_code=404)
+    if "username" in request_body_user:
+        user1.username = body["username"]
+    if "email" in request_body_user:
+        user1.email = body["email"]
+    if "first_name" in request_body_user:
+        user1.first_name = request_body_user["first_name"]
+    db.session.commit()
+    return jsonify(request_body_user), 200
+
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+
+    request_body_user = request.get_json()
+    user1 = User.query.get(user_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+    return jsonify(request_body_user), 200
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_character(character_id):
+
+        request_body_character = request.get_json()
+        character1 = Character.query.get(character_id)
+        return jsonify(character1.serialize()), 200
+
+
+@app.route('/character', methods=['GET'])
+def get_characters():
+
+    characters = Character.query.all()
+    all_characters = list(map(lambda x: x.serialize(), characters))
+
+    return jsonify(all_characters), 200
+
+
+@app.route('/character', methods=['POST'])
+def add_character():
+
+    request_body_character = request.get_json()
+    character1 = Character(
+    name=request_body_character["name"],
+    birth_year=request_body_character["birth_year"]
+    )
+    db.session.add(character1)
+    db.session.commit()
+    return jsonify(request_body_character), 200
+
+@app.route('/character/<int:character_id>', methods=['DELETE'])
+def delete_character(character_id):
+
+    request_body_character = request.get_json()
+    character1 = Character.query.get(character_id)
+    if character1 is None:
+        raise APIException('Character not found', status_code=404)
+    db.session.delete(character1)
+    db.session.commit()
+    return jsonify(request_body_character), 200
+
+@app.route('/planet', methods=['GET'])
+def get_planets():
+
+    planets = Planet.query.all()
+    all_planets = list(map(lambda x: x.serialize(), planets))
+
+    return jsonify(all_planets), 200
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+
+    request_body_planet = request.get_json()
+    planet1 = Planet.query.get(planet_id)
+    return jsonify(planet1.serialize()), 200
+
+@app.route('/planet', methods=['POST'])
+def add_planet():
+
+    request_body_planet = request.get_json()
+    planet1 = Planet(
+    name=request_body_planet["name"],
+    climate=request_body_planet["climate"]
+    )
+    db.session.add(planet1)
+    db.session.commit()
+    return jsonify(request_body_planet), 200
+
+@app.route('/user/<int:user_id>/favorite/character/<int:character_id>', methods=['GET', 'POST'])
+def fav_characters(user_id, character_id):
+    
+    if request.method=='POST':
+        request_body_character = request.get_json()
+        character1 = Character.query.get(character_id)
+        new_favorite= CharacterFavorite(user_id=user_id, character_id=character_id)
+        db.session.add(new_favorite)
+        db.session.commit()
+        return jsonify(request_body_character), 200
+    else: 
+        favorites=CharacterFavorite.query.filter_by(user_id=user_id).all()
+        all_characters = list(map(lambda x: x.serialize(), favorites))
+        return jsonify(all_characters), 200
+
+
+@app.route('/user/<int:user_id>/favorite/character/<int:character_id>', methods=['DELETE'])
+def delete_fav_character(user_id, character_id):
+
+        request_body_character = request.get_json()
+        character1 = Character.query.get(character_id)
+        if character1 is None:
+            raise APIException('Character not found', status_code=404)
+        db.session.delete(character1)
+        db.session.commit()
+        return jsonify(request_body_character), 200
+
+@app.route('/user/<int:user_id>/favorite/planet/<int:planet_id>', methods=['GET', 'POST'])
+def fav_planets(user_id, planet_id):
+    
+    if request.method=='POST':
+        request_body_planet = request.get_json()
+        planet1 = Planet.query.get(planet_id)
+        new_favorite= PlanetFavorite(user_id=user_id, planet_id=planet_id)
+        db.session.add(new_favorite)
+        db.session.commit()
+        return jsonify(request_body_planet), 200
+    else: 
+        favoritesp=PlanetFavorite.query.filter_by(user_id=user_id).all()
+        all_planets = list(map(lambda x: x.serialize(), favoritesp))
+        return jsonify(all_planets), 200
+
+@app.route('/user/<int:user_id>/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_fav_planet(user_id, character_id):
+
+        request_body_planet = request.get_json()
+        planet1 = Planet.query.get(planet_id)
+        if planet1 is None:
+            raise APIException('Planet not found', status_code=404)
+        db.session.delete(planet1)
+        db.session.commit()
+        return jsonify(request_body_planet), 200
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,12 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True, default=True)
+    # character_favorite = db.relationship('CharacterFavorite', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +18,75 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+        }
+            # do not serialize the password, its a security breach
+        
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    birth_year = db.Column(db.String(80), nullable=False)
+    character_favorite = db.relationship('CharacterFavorite', lazy=True)
+  
+    def __repr__(self):
+        return '<Character %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            # do not serialize the password, its a security breach
+        }
+
+class CharacterFavorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship('User', lazy=True)
+    character_id = db.Column(db.Integer, db.ForeignKey("character.id"))
+    character = db.relationship('Character', lazy=True )
+       
+    def __repr__(self):
+        return '<Character %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character.id": self.character_id,
+         
+            # do not serialize the password, its a security breach
+        }
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    climate = db.Column(db.String(80), nullable=False)
+    planet_favorite = db.relationship('PlanetFavorite', lazy=True)
+  
+    def __repr__(self):
+        return '<Planet %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            # do not serialize the password, its a security breach
+        }
+
+class PlanetFavorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user = db.relationship('User', lazy=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"))
+    planet = db.relationship('Planet', lazy=True )
+       
+    def __repr__(self):
+        return '<Planet %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet.id": self.planet_id, 
             # do not serialize the password, its a security breach
         }
